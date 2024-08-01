f29251cef82a2ad35bcad3e8e0b1da2b1d1c5712
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 17:05:30 +1000

    Check SEARCH_ON filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert Snowpark Dataframe to a Pandas Dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -49,6 +49,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
