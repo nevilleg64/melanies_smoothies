26d9a3f8cd6cdba39fa220b2635bd1c7c22b419a
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 16:25:46 +1000

    Move fruityvice call to for-loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
+import requests
 import streamlit as st
+
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie! :balloon:")
@@ -35,16 +36,15 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
     st.write(ingredients_string)
     
