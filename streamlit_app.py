64023f716f665e2ec32a8f42e153413d7e83ba3a
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 16:22:09 +1000

    Show fruitvice in dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ ingredients_list = st.multiselect(
 )
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
     # st.write(ingredients_list)
