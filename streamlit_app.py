2329e189de3888cc10397ea7d3de981f837dfd95
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 16:28:38 +1000

    Add nutrition info for each selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
