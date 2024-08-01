dfd6a44b31057f7d8ad1dc128e37ad3f3cebf53d
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 16:15:53 +1000

    Update streamlit_app.py
    Add API call to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie! :balloon:")
@@ -33,6 +34,9 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 if ingredients_list:
     # st.write(ingredients_list)
