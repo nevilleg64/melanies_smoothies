d6d342393b344d49a1805ae40417954c65d5957b
Neville Goldring <nevilleg64@gmail.com>
2024-08-01 17:12:25 +1000

    Fix nutrition issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
